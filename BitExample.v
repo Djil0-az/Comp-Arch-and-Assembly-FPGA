9ba2cbf61cd0d92657a1448ada90b08b3420eba8
Jalal <109775640+Djil0-az@users.noreply.github.com>
2023-09-24 13:33:52 +0800

    Update BitExample.v

--- BitExample.v
+++ BitExample.v
@@ -37,7 +37,7 @@
     
     integer  clk_cnt_tmp = 0;
     reg clk_tmp = 0;
-    reg [4:0] bat_ctl;
+    wire [4:0] bat_ctl;
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n)
             clk_tmp <= 1'b0;
@@ -57,30 +57,6 @@
         // Connect other ports as needed
         
 );
-initial begin
-        mips.Mem[0] = {6'd10, 5'd0, 5'd31, 16'd1};          // R32 = 1
-        mips.Mem[1] = {6'd10, 5'd0, 5'd31, 16'd2};          // R32 = 2
-        mips.Mem[2] = {6'd10, 5'd0, 5'd31, 16'd1};          // R32 = 3
-        mips.Mem[3] = {6'd10, 5'd0, 5'd31, 16'd2};          // R32 = 4
-        mips.Mem[4] = {6'd10, 5'd0, 5'd31, 16'd1};          // R32 = 5
-        mips.Mem[5] = {6'd10, 5'd0, 5'd31, 16'd2};          // R32 = 6
-        mips.Mem[6] = {6'd10, 5'd0, 5'd31, 16'd1};          // R32 = 7
-        mips.Mem[7] = {6'd10, 5'd0, 5'd31, 16'd2};          // R32 = 8
-        mips.Mem[8] = {6'd10, 5'd0, 5'd31, 16'd1};          // R32 = 9
-        mips.Mem[9] = {6'd10, 5'd0, 5'd31, 16'd2};          // R32 = 10
-        mips.Mem[10] = {6'd10, 5'd0, 5'd31, 16'd1};          // R32 = 11
-        mips.Mem[11] = {6'd10, 5'd0, 5'd31, 16'd2};          // R32 = 12
-        mips.Mem[12] = {6'd10, 5'd0, 5'd31, 16'd1};          // R32 = 13
-        mips.Mem[13] = {6'd10, 5'd0, 5'd31, 16'd2};          // R32 =14
-        mips.Mem[14] = {6'd10, 5'd0, 5'd31, 16'd1};          // R32 = 15
-        mips.Mem[0] = {6'd10, 5'd0, 5'd31, 16'd1};          // R32 = 16
-   
-end
-
-   
-    always @(posedge clk_tmp) begin
-        bat_ctl = bat_ctl + 1;
-    end
 
     
     vga_char vga0(    .clk(clk), .rst_n(rst_n), .bat_ctl(bat_ctl), .hsync(hsync), .vsync(vsync),.r(r),.g(g),.b(b));
